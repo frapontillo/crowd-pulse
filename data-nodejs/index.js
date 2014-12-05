2c48751095c62c199db408f58640efc146006052
Francesco Pontillo <francescopontillo@gmail.com>
2014-12-05 18:59:13 +0100

    style: update copyright

--- data-nodejs/index.js
+++ data-nodejs/index.js
@@ -1,36 +1,80 @@
 /*
  * Copyright 2014 Francesco Pontillo
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
+var Q = require('q');
 var mongoose = require('mongoose');
 var AppStore = require('./store/app')(mongoose);
 
-mongoose.connect('mongodb://localhost/test');
+// make mongoose Promise/A+ compliant
+require('mongoose-q')(mongoose);
 
-var onOpen = function() {
-  AppStore.findOne({ name: 'test' }).exec()
-    .then(function (result) {
-      return result || AppStore.create({
-        name: 'test',
-        secret: 'yolo'
+var DataLayer = function() {
+
+  var getCallbackAsPromiseFn = function(promise, resolveThis) {
+    return function(err) {
+      if (err) {
+        promise.error(err);
+      } else {
+        promise.resolve(resolveThis);
+      }
+    };
+  };
+
+  var self = this;
+
+  this.connect = function(host, database, port, options) {
+    var connectionPromise = Q.defer();
+    self.connection = mongoose.createConnection();
+
+    self.connection.open(host, database, port, options, getCallbackAsPromiseFn(connectionPromise, self.connection));
+    return connectionPromise.promise;
+  };
+
+  this.disconnect = function() {
+    var disconnectionPromise = Q.defer();
+
+    mongoose.disconnect(getCallbackAsPromiseFn(disconnectionPromise, self.connection));
+    return disconnectionPromise.promise;
+  };
+
+  this.initDatabase = function() {
+    AppStore.findOneQ({ name: 'test' })
+      .then(function (result) {
+        return result || AppStore.createQ({
+          name: 'test',
+          secret: 'yolo'
+        });
       });
-    })
-    .then(function (a) {
-      console.log(a);
-      return a;
-    });
+  };
 };
 
-mongoose.connection.once('open', onOpen);
+var data = new DataLayer();
+
+data.connect('mongodb://localhost/test')
+  .then(function() {
+    return data.initDatabase();
+  })
+  .then(function() {
+    return data.disconnect();
+  })
+  .finally(function() {
+    console.log('we\'re done');
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+
+console.log('yolo');
