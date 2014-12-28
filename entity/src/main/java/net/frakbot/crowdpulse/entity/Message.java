b343f6115060f7d923c73a19527033a4e63f8073
Francesco Pontillo <francescopontillo@gmail.com>
2014-12-28 15:19:14 +0100

    feat(entity): remove IExtractor, add Message entity

---
+++ entity/src/main/java/net/frakbot/crowdpulse/entity/Message.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2014 Francesco Pontillo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.frakbot.crowdpulse.entity;
+
+import java.util.Date;
+
+/**
+ * @author Francesco Pontillo
+ */
+public class Message {
+    private String text;
+    private String source;
+    private String fromUser;
+    private String toUser;
+
+    private Date date;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getFromUser() {
+        return fromUser;
+    }
+
+    public void setFromUser(String fromUser) {
+        this.fromUser = fromUser;
+    }
+
+    public String getToUser() {
+        return toUser;
+    }
+
+    public void setToUser(String toUser) {
+        this.toUser = toUser;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
