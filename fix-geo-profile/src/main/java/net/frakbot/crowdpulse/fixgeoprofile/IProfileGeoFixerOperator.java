211c01972a8b79ac75799d02d91f97bf6e929fd1
Francesco Pontillo <francescopontillo@gmail.com>
2015-08-17 12:31:56 +0200

    docs(fix-geo-profile): complete documentation
    Addresses #14

--- fix-geo-profile/src/main/java/net/frakbot/crowdpulse/fixgeoprofile/IProfileGeoFixerOperator.java
+++ fix-geo-profile/src/main/java/net/frakbot/crowdpulse/fixgeoprofile/IProfileGeoFixerOperator.java
@@ -23,6 +23,11 @@ import rx.Observable;
 import rx.Subscriber;
 
 /**
+ * Rx {@link rx.Observable.Operator} that accepts and outputs {@link Profile}s after attempting a geo-location fix on
+ * them.
+ * <p>
+ * Clients should implement {@link #getCoordinates(Profile)}.
+ *
  * @author Francesco Pontillo
  */
 public abstract class IProfileGeoFixerOperator implements Observable.Operator<Profile, Profile> {
@@ -55,6 +60,13 @@ public abstract class IProfileGeoFixerOperator implements Observable.Operator<Pr
         };
     }
 
+    /**
+     * Fixes the geo-location of a {@link Profile} by calling the actual {@link #getCoordinates(Profile)}
+     * implementation.
+     *
+     * @param profile The {@link Profile} to fix the geo-location for.
+     * @return The same input {@link Profile} with an eventual geo-location set to it.
+     */
     protected Profile geoFixProfile(Profile profile) {
         Double[] coordinates = getCoordinates(profile);
         if (coordinates != null && coordinates.length == 2) {
@@ -66,6 +78,7 @@ public abstract class IProfileGeoFixerOperator implements Observable.Operator<Pr
 
     /**
      * Actual retrieval of {@link Profile} coordinates happens here.
+     *
      * @param profile The {@link Profile} to retrieve coordinates for.
      * @return Array of {@link Double} containing, in order, latitude and longitude.
      */
