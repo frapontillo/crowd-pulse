08c347ac93290353432a79a52712b4000bd16468
Francesco Pontillo <francescopontillo@gmail.com>
2015-10-10 19:10:34 +0200

    feat(data-node): search tags, categories, tokens

--- data-node/schema/message.js
+++ data-node/schema/message.js
@@ -16,6 +16,7 @@
 
 'use strict';
 
+var Q = require('q');
 var mongoose = require('mongoose');
 var builder = require('./schemaBuilder');
 var schemas = require('./schemaName');
@@ -40,5 +41,52 @@ var MessageSchema = builder(schemas.message, {
   tokens: [schemas.token],
   sentiment: Number
 });
+
+var buildSearchQuery = function(type, search) {
+  var queryOn;
+  var unwinds = 1;
+  if (type === 'tag') {
+    queryOn = 'tags._id';
+  } else if (type === 'token') {
+    queryOn = 'tags._id';
+  } else if (type === 'category') {
+    // categories are two levels nested
+    queryOn = 'tags.categories.text';
+    unwinds = 2;
+  } else {
+    throw new Error('"' + type + '" is an unknown search type.');
+  }
+  var regex = new RegExp(search, 'i');
+  // match all messages with at least one element matching
+  var matchFirstStep = {
+    '$match': {}
+  };
+  matchFirstStep['$match'][queryOn] = {$regex: regex, $options: 'i'};
+  // project only elements of interest
+  var projectSecondStep = {
+    $project: {_id: false, 'item': '$' + queryOn}
+  };
+  // unwind all elements from arrays to single values (n levels of nesting = n unwinds)
+  var unwindStep = {$unwind: '$item'};
+  var steps = [
+    matchFirstStep,
+    projectSecondStep
+  ];
+  for (var i = 0; i < unwinds; i++) {
+    steps.push(unwindStep);
+  }
+  // remove duplicate values, re-search on the search parameter, finally sort elements
+  steps.push(
+    {$group: {_id: "$item"}},
+    {$match: {_id: {$regex: regex, $options: 'i'}}},
+    {$sort: {_id: 1}}
+  );
+  return steps;
+};
+
+MessageSchema.statics.searchTerm = function(type, term) {
+  var model = this;
+  return Q(model.aggregate(buildSearchQuery(type, term)).exec());
+};
 
 module.exports = MessageSchema;
