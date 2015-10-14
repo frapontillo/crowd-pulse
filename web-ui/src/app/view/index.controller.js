73124ff9aa7ae68fc8bdabe1b36ca0e5681b064e
Francesco Pontillo <francescopontillo@gmail.com>
2015-10-14 17:24:13 +0200

    feat(web-ui): add bar/pie chart for word distribution (tag, token, category)
    Close #26

--- web-ui/src/app/view/index.controller.js
+++ web-ui/src/app/view/index.controller.js
@@ -21,13 +21,160 @@
     .module('webUi')
     .controller('ViewIndexController', ViewIndexController);
 
+  /* global Highcharts:false */
   /** @ngInject */
-  function ViewIndexController() {
+  function ViewIndexController($scope, $timeout, $window, Stat) {
     var vm = this;
 
-    // select all types of filter
-    vm.params = {
+    vm.params = {};
+
+    var buildBaseHighcharts = function(type) {
+      return {
+        chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: type
+        },
+        title: {
+          text: null
+        },
+        exporting: {
+          buttons: {
+            contextButton: {
+              enabled: false
+            }
+          }
+        },
+        credits: {
+          enabled: false
+        },
+        legend: {
+          enabled: false
+        }
+      };
     };
+
+    var buildPieChart = function(title, values) {
+      var chart = buildBaseHighcharts('pie');
+      chart.tooltip = {
+        pointFormat: '{series.name}: <b>{point.y} ({point.percentage:.1f}%)</b>'
+      };
+      chart.plotOptions = {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}</b>: {point.y} ({point.percentage:.1f}%)',
+            style: {
+              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+            }
+          }
+        }
+      };
+      chart.series = [{
+        name: title,
+        colorByPoint: true,
+        data: values
+      }];
+      return chart;
+    };
+
+    var buildBarChart = function(xTitle, xValues, yTitle, yValues) {
+      var chart = buildBaseHighcharts('bar');
+      chart.tooltip = {
+        pointFormat: '{series.name}: <b>{point.y}</b>'
+      };
+      chart.xAxis = {
+        categories: xValues,
+        title: {
+          text: xTitle
+        }
+      };
+      chart.yAxis = {
+        title: {
+          text: yTitle
+        },
+        labels: {
+          overflow: 'justify'
+        }
+      };
+      chart.series = [{
+        name: yTitle,
+        colorByPoint: true,
+        data: yValues
+      }];
+      return chart;
+    };
+
+    var buildStatParams = function() {
+      return {
+        db: vm.params.database,
+        type: vm.params.filterOn,
+        terms: vm.params.query,
+        from: vm.params.fromDate,
+        to: vm.params.toDate
+      };
+    };
+
+    var getStatWords = function() {
+      return Stat.Terms.getList(buildStatParams());
+    };
+
+    var statToPieMap = function(stats) {
+      return stats.map(function(stat) {
+        return {
+          name: stat.name,
+          y: stat.value
+        };
+      });
+    };
+
+    var statToBarMap = function(stats) {
+      var cats = stats.map(function(stat) {
+        return stat.name;
+      });
+      var series = stats.map(function(stat) {
+        return stat.value;
+      });
+      return [cats, series];
+    };
+
+    var statWordPie = function() {
+      return getStatWords()
+        .then(statToPieMap)
+        .then(function(stats) {
+          vm.stat = buildPieChart('Occurrences', stats);
+        });
+    };
+
+    var statWordBar = function() {
+      return getStatWords()
+        .then(statToBarMap)
+        .then(function(categoriesSeries) {
+          vm.stat = buildBarChart(vm.params.filterOn, categoriesSeries[0], 'Occurrences', categoriesSeries[1]);
+        });
+    };
+    var handlers = {
+      'word-pie': statWordPie,
+      'word-bar': statWordBar,
+    };
+
+    $scope.$watch('vm.params', function(newValue, oldValue) {
+      if (newValue === oldValue) {
+        return;
+      }
+      vm.stat = null;
+      handlers[newValue.dataViz]()
+        .then(function() {
+          // forcefully dispatch a resize event to make the word cloud recalculate its dimensions
+          $timeout(function() {
+            /* global Event:false */
+            $window.dispatchEvent(new Event('resize'));
+          });
+        });
+    }, true);
   }
 
 })();
