78fb3602d1fee9cc8760d5e1af23194a9f776521
Francesco Pontillo <francescopontillo@gmail.com>
2015-11-08 16:43:41 +0100

    feat(web-service): enable download of messages in database

--- web-service/endpoint/databases.js
+++ web-service/endpoint/databases.js
@@ -19,12 +19,44 @@
 var qSend = require('../lib/expressQ').send;
 var qErr = require('../lib/expressQ').error;
 var router = require('express').Router();
+var CrowdPulse = require('crowd-pulse-data-node');
+var config = require('./../config.json');
 
 module.exports = function(crowdPulse) {
 
   router.route('/databases')
     .get(function(req, res) {
       return crowdPulse.getDatabases().then(qSend(res)).catch(qErr(res));
+    });
+
+  router.route('/databases/:id')
+    // /api/databases/123122?author=frapontillo&language=it
+    .get(function(req, res) {
+      var dbConn = new CrowdPulse();
+      return dbConn.connect(config.database.url, req.params.id)
+        .then(function(conn) {
+          return conn.Message.search(req.query.author, req.query.language);
+        })
+        .then(function(messages) {
+          var filename = req.params.id;
+          if (req.query.author) {
+            filename += '-' + req.query.author;
+          }
+          if (req.query.language) {
+            filename += '-' + req.query.language;
+          }
+          filename += '.json';
+          res.set({
+            'Content-Disposition': 'attachment; filename=' + filename,
+            'Content-type': 'application/json'
+          });
+          return messages;
+        })
+        .then(qSend(res))
+        .catch(qErr(res))
+        .finally(function() {
+          dbConn.disconnect();
+        });
     });
 
   return router;
